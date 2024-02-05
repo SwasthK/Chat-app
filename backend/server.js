7fc7425c04e91d906082a9460470985e37be91c9
SwasthK <220981@sdmcujire.in>
2024-02-05 23:58:34 +0530

    ADD Main Route

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const user = require('./routes/user')
+const Usermsg = require('./routes/messages')
+
+const connectmongo = require('./db/connect')
+
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/chat/api/v1', user)
+app.use('/chat/api/v1', Usermsg)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, (req, res) => {
+    connectmongo();
+    console.log(`server is listening at port ${port}`);
+})
