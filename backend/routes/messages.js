213c1459de4b3927e33437006dd0d065c09b9df5
SwasthK <220981@sdmcujire.in>
2024-02-06 23:29:28 +0530

    ADD Route to fetch all users

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const sendmsg = require('../controllers/msgcontrol')
 const authmiddleware = require('../middleware/messageauth')
+const { sendmsg, getmessages } = require('../controllers/msgcontrol')
 
 const Usermsg = express.Router()
 
+Usermsg.get('/message/get/:id', authmiddleware, getmessages)
 Usermsg.post('/message/send/:id', authmiddleware, sendmsg)
 
 module.exports = Usermsg
