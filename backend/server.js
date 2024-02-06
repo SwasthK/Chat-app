d2703fc9a52b618b97c0a94720e64ef37c90453c
SwasthK <220981@sdmcujire.in>
2024-02-06 23:39:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ const Usermsg = require('./routes/messages')
 const connectmongo = require('./db/connect')
 
 const cookieParser = require('cookie-parser')
+const alluserRouter = require('./routes/globalusers')
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
 
+app.use('/chat/api/v1', alluserRouter)
 app.use('/chat/api/v1', user)
 app.use('/chat/api/v1', Usermsg)
 
